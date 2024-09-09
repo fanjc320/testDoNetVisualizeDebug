7d191e2d630906e6f4f281c4353a44b987bdfb2b
jackyfan <894020798@qq.com>
2024-09-09 11:54:33 +0800

    second commit

---
+++ MyFirstVisualizer/DebuggerSide.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+using System.Windows.Forms;
+
+[assembly: System.Diagnostics.DebuggerVisualizer(
+typeof(MyFirstVisualizer.DebuggerSide),
+typeof(VisualizerObjectSource),
+Target = typeof(System.String),
+Description = "My First Visualizer")]
+namespace MyFirstVisualizer
+{
+    public class DebuggerSide : DialogDebuggerVisualizer
+    {
+        public DebuggerSide() : base(FormatterPolicy.NewtonsoftJson) // or FormatterPolicy.Json
+        {
+        }
+
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            MessageBox.Show(objectProvider.GetObject().ToString());
+        }
+
+        public static void TestShowVisualizer(object objectToVisualize)
+        {
+            VisualizerDevelopmentHost visualizerHost = new VisualizerDevelopmentHost(objectToVisualize, typeof(DebuggerSide));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+}
---
+++ MyTestConsole/TestConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyFirstVisualizer;
+
+namespace MyTestConsole
+{
+    internal static class TestConsole
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            String myString = "Hello, World";
+            DebuggerSide.TestShowVisualizer(myString);
+
+            Application.Run(new Form1());
+        }
+    }
+}
